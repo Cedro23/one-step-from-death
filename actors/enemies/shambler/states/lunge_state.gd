404c3cebd2d513bf4b18e0f5e0f9e6b00b2cd09b
Cedro23 <cedric_lesueur@outlook.fr>
2026-03-22 16:22:25 +0100

    feat: shambler now lunges

--- actors/enemies/shambler/states/lunge_state.gd
+++ actors/enemies/shambler/states/lunge_state.gd
@@ -1,21 +1,36 @@
 extends State
 
-const LUNGE_SPEED: float = 200.0
+const LUNGE_SPEED: float = 300.0
 @onready var lunge_windup_timer: Timer = $"../../LungeWindupTimer"
 @onready var lunge_timer: Timer = $"../../LungeTimer"
 
+const HITBOX_DISTANCE: float = 16.0
+@onready var hitbox: Area2D = $"../../Hitbox"
+@onready var hitbox_collision: CollisionShape2D = $"../../Hitbox/CollisionShape2D"
+
+var player: CharacterBody2D
+var direction: Vector2
+var rotation: float
+
 func enter() -> void:
+	player = character.get_node("/root/Main/Player")
+	direction = (player.global_position - character.global_position).normalized()
+	rotation = player.global_position.angle_to_point(character.global_position)
+
+	hitbox.position = direction * HITBOX_DISTANCE
+	hitbox.rotation = rotation
 	character.velocity = Vector2.ZERO
+	
 	lunge_windup_timer.start()
 
 func physics_update(_delta: float) -> void:
 	character.move_and_slide()
 
-
 func _on_lunge_windup_timer_timeout() -> void:
-	# Lock in on player
-	# And lunge
+	hitbox_collision.disabled = false
+	character.velocity = direction * LUNGE_SPEED
 	lunge_timer.start()
 
 func _on_lunge_timer_timeout() -> void:
+	hitbox_collision.disabled = true
 	character.state_machine.transition_to("RecoveryState")
--- actors/enemies/shambler/states/recovery_state.gd
+++ actors/enemies/shambler/states/recovery_state.gd
@@ -3,7 +3,7 @@ extends State
 @onready var recovery_timer: Timer = $"../../RecoveryTimer"
 
 func enter() -> void:
-	# Start revocery timer
+	character.velocity = Vector2.ZERO
 	recovery_timer.start()
 
 func _on_recovery_timer_timeout() -> void:
