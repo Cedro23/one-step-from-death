95548a88e25881296315d6cbd4fe67b437d3940c
Cedro23 <cedric_lesueur@outlook.fr>
2026-03-23 11:52:07 +0100

    feat: exit doors handling and enemy counting

--- rooms/room.gd
+++ rooms/room.gd
@@ -4,12 +4,16 @@ extends Node2D
 @onready var bounds_bottom_right = $BoundsBottomRight
 @onready var room_area = $RoomArea
 
+@onready var doors: Array[Node]
+
 var is_player_inside: bool = false
+var enemies_alive: int = 0
 
 func _ready() -> void:
 	_setup_bounds()
 	_place_player()
-	# _count_enemies()
+	_count_enemies()
+	doors = get_tree().get_nodes_in_group("door")
 
 func _setup_bounds() -> void:
 	var top_left = bounds_top_left.position
@@ -27,10 +31,15 @@ func _place_player() -> void:
 	var player = game_manager.player
 	player.global_position = $SpawnPoint.global_position
 
+func _count_enemies() -> void:
+	for enemy in $Enemies.get_children():
+		enemies_alive += 1
+		enemy.tree_exited.connect(_on_enemy_died)
+
 func _on_room_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		is_player_inside = true
-		# lock_exists()
+		_toggle_exits(false)
 		_setup_camera(body)
 
 func _setup_camera(player: Node) -> void:
@@ -39,3 +48,12 @@ func _setup_camera(player: Node) -> void:
 	camera.limit_top = int(bounds_top_left.position.y)
 	camera.limit_right = int(bounds_bottom_right.position.x)
 	camera.limit_bottom = int(bounds_bottom_right.position.y)
+
+func _toggle_exits(open: bool) -> void:
+	for door in doors:
+		door.get_node("CollisionShape2D").disabled = open
+
+func _on_enemy_died() -> void:
+	enemies_alive = max(0, enemies_alive - 1)
+	if enemies_alive == 0 and is_player_inside:
+		_toggle_exits(true)
