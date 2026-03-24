6e37e03432736af7ae742f6d2f16178ba70632ad
Cedro23 <cedric_lesueur@outlook.fr>
2026-03-24 10:35:42 +0100

    fix: set engine time scale to 1.0 when restarting the run

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -2,11 +2,12 @@ extends Node
 
 const PLAYER_SCENE = preload("res://actors/player/player.tscn")
 const FIRST_ROOM = "res://rooms/floor_1/room_01.tscn"
+const FIRST_SPAWN_DIRECTION = "east"
 
 var player: CharacterBody2D
 var room_states: Dictionary = {}
 var current_room_path: String = ""
-var incoming_spawn_direction: String = "east"
+var incoming_spawn_direction: String = FIRST_SPAWN_DIRECTION
 
 func _ready():
 	player = PLAYER_SCENE.instantiate()
@@ -27,7 +28,7 @@ func go_to_room(destination: String, spawn_direction: String) -> void:
 func restart_run() -> void:
 	room_states.clear()
 	current_room_path = ""
-	incoming_spawn_direction = "east"
-	# Engine.time_scale = 1.0
+	incoming_spawn_direction = ""
+	Engine.time_scale = 1.0
 	player.get_node("StateMachine").transition_to("IdleState")
-	go_to_room(FIRST_ROOM, "east")
+	go_to_room(FIRST_ROOM, FIRST_SPAWN_DIRECTION)
